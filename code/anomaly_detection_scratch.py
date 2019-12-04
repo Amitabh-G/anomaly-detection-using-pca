3687f7e34a73d5cb38f38ae01f015ffd0a3f4a5c
TheHBK <37610177+TheHBK@users.noreply.github.com>
2019-12-04 20:45:13 +0530

    PCA fit

--- code/anomaly_detection_scratch.py
+++ code/anomaly_detection_scratch.py
@@ -8,27 +8,32 @@ Created on Wed Dec  4 12:44:46 2019
 
 """
 Code for anomaly detection using PCA.
+
+	3) Perform PCA over the data.  
+	4) Use the anomaly detection logic to get the anomalies after doing PCA.
+    5) Create a function to get the metrics related to the classification.
+
 """
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 
-with open('D:/work/anomaly-detection/kdd-data/colNames.txt', 'r') as f:
+with open('D:/other/Programming/python/anomaly-detection-using-pca/kdd-data/colNames.txt', 'r') as f:
     col_names = f.readlines()
 
 col_names = [i.split(":")[0] for i in col_names]
 col_names.append("class")
-kdd_data = pd.read_csv("D:/work/anomaly-detection/kdd-data/kddcup.data_10_percent.csv")
-# Filter normal data points
+kdd_data = pd.read_csv("D:/other/Programming/python/anomaly-detection-using-pca/kdd-data/kddcup.data_10_percent.csv")
 
-#kdd_data = kdd_data.iloc[:,:-1]
+# Filter normal data points
 kdd_data.columns = col_names
 kdd_data_normal = kdd_data[kdd_data['class']=='normal.']
-
 kdd_data_normal_numeric = kdd_data_normal.select_dtypes(include=['number'])
 
-
+X_train, X_test = train_test_split(kdd_data_normal_numeric, test_size=0.33, random_state=42)
 pca = PCA()
-fit = pca.fit(kdd_data_normal_numeric)
-np.shape(fit.components_)
-fit.transform()
+fit = pca.fit(X_train)
+fit_ = fit.transform(X_test)
+fit_[0]
+
