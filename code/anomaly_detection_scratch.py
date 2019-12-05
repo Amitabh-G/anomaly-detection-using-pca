9157f7e9eb3b2d07baf78b8eff394d5536a87613
TheHBK <37610177+TheHBK@users.noreply.github.com>
2019-12-05 16:51:39 +0530

    Anomaly detection logic based on sample quantiles of standardized principal component scores or coordinates in the prcomp space

--- code/anomaly_detection_scratch.py
+++ code/anomaly_detection_scratch.py
@@ -18,10 +18,14 @@ import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
+from statsmodels.distributions.empirical_distribution import ECDF
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 with open('D:/other/Programming/python/anomaly-detection-using-pca/kdd-data/colNames.txt', 'r') as f:
     col_names = f.readlines()
-
 col_names = [i.split(":")[0] for i in col_names]
 col_names.append("class")
 kdd_data = pd.read_csv("D:/other/Programming/python/anomaly-detection-using-pca/kdd-data/kddcup.data_10_percent.csv")
@@ -30,10 +34,52 @@ kdd_data = pd.read_csv("D:/other/Programming/python/anomaly-detection-using-pca/
 kdd_data.columns = col_names
 kdd_data_normal = kdd_data[kdd_data['class']=='normal.']
 kdd_data_normal_numeric = kdd_data_normal.select_dtypes(include=['number'])
-
+ 
 X_train, X_test = train_test_split(kdd_data_normal_numeric, test_size=0.33, random_state=42)
 pca = PCA()
 fit = pca.fit(X_train)
-fit_ = fit.transform(X_test)
-fit_[0]
 
+def get_empirical_distribution(X_train):
+    """
+    Get the empirical quantiles of the sum of ratio of principal component scores 
+    and the eigenvalues using the observations from the training dataset.
+    From the paper - http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.66.299&rep=rep1&type=pdf
+    """      
+
+    evals = fit.explained_variance_
+    ss_standardized_prcomp_scores = []
+    fit_transform = fit.transform(X_train)
+    for i in fit_transform:
+        prcomp_obs_sq = np.square(i)
+        ss_prcomp_scores = np.sum((np.divide(prcomp_obs_sq, evals)))
+        ss_standardized_prcomp_scores.append(ss_prcomp_scores)  
+        
+    return ss_standardized_prcomp_scores
+
+ecdf = get_empirical_distribution(X_train)
+
+def get_ecdf(ss_prcomp_scores, val):
+    """
+    Compare an observation's sum of ratio of principal component scores 
+    and the eigenvalues using the observations from the training dataset to the empirical quantiles.
+    """
+    ecdf = ECDF(ss_prcomp_scores)
+    quantile = ecdf(val)
+    return quantile
+
+print(get_ecdf(ss_prcomp_scores=ecdf, val=[50]))
+
+# Get the non-normal instances. Perform classification over these.
+# Bucket all different kinds of non-normal observations into one.
+kdd_data_non_normal = kdd_data[kdd_data['class'] !='normal.']
+kdd_data_non_normal_numeric = kdd_data_non_normal.select_dtypes(include=['number'])
+Y = kdd_data_non_normal['class']
+X_train_nn, X_test_nn, Y_train_nn, Y_test_nn = train_test_split(kdd_data_non_normal_numeric, Y, test_size=0.33, random_state=42)
+prcomp_scores = get_empirical_distribution(X_test_nn)
+quantiles_scores = get_ecdf(ss_prcomp_scores=ecdf, val=prcomp_scores)
+
+anomalies_ = [True if i > 0.9899 else False for i in quantiles_scores ]
+Y_true = [True for i in Y_test_nn]
+confusion_matrix(Y_true, anomalies_)
+precision_score(Y_true, anomalies_)
+recall_score(Y_true, anomalies_)
